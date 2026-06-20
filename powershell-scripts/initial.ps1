81bb4172e5451c0b2ec746e95a1846f66c22a95f
Laksh <raftaarmalhotra@gmail.com>
2026-06-20 12:15:09 +0530

    update: initial ps1 partially done

--- powershell-scripts/initial.ps1
+++ powershell-scripts/initial.ps1
@@ -1,12 +1,19 @@
 $Password = ConvertTo-SecureString "123456" -AsPlainText -Force
 $NewUser = "test"
+
 New-LocalUser -Name $NewUser -Password $Password -Description "NewAdminAccount"
+Write-Host "New Account created" -ForegroundColor Green 
+
 Set-LocalUser -Name $NewUser -PasswordNeverExpires $true
+Write-Host "Set password to never expire" -ForegroundColor Green
 
-net user xyz /passwordchg:no
+net $NewUser xyz /passwordchg:no
+Write-Host "Password change disabled" -ForegroundColor Green
 
-Add-LocalGroupMember -Group "Administrators" -Member "test"
+Add-LocalGroupMember -Group "Administrators" -Member $NewUser
+Write-Host "Added to admin group" -ForegroundColor Green
 
 Disable-LocalUser -Name "OldUserAccount"
+Write-Host "Disbled current account" -ForegroundColor Green
 
-Write-Host "Script 1 complete. Please logout and login into user $NewUser to run final script." -ForgroundColor Green
+Write-Host "Script 1 complete. Please logout and login into user $NewUser to run final script." -ForegroundColor Green
